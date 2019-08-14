cecce8ba78e5302e274b1ecb0904c6d3134daa65
lacalepfl <x>
2019-08-14 11:02:36 +0200

    adapt to new definition

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Script to verify the data of a unicorn beacon output.
 
 The script:
   - implemented with python 3
-  - need 3 files:
+  - need 4 files:
     - the seed file
     - the file with values
     - the image file
+    - the encrypted image file
 
 
 a example of file is given
@@ -15,8 +16,9 @@ a example of file is given
 the values file is copy-paste of the site
 the seeds file can be download at the same place
 the image file can be download at the same place
+the encrypted image file can be download at the same place
 
 
 running:
-python3 checker.py seedsfile valuefile imagefile
+python3 checker.py seedsfile valuefile imagefile encryptedimagefile
 
--- checker.py
+++ checker.py
@@ -3,6 +3,7 @@ import hashlib
 import random
 import subprocess
 import numpy as np
+import filecmp
 
 TEXT_NOT_VALID="Verification FAILLURE, values does not correspond"
 TEXT_VALID="Verification SUCCESS, all values are good"
@@ -15,6 +16,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('path_to_tweetfile')
 parser.add_argument('path_to_valuefile')
 parser.add_argument('path_to_imagefile')
+parser.add_argument('path_to_encryptimagefile')
 args = parser.parse_args()
 
 number=0
@@ -28,22 +30,18 @@ COMMIT_Q=0
 with open(args.path_to_valuefile) as valuefile:
     listvalue=valuefile.readlines()
     for line in listvalue:
-        split=line.split(' : ')
-        if split[0] == "Random Number":
+        split=line.split(': ')
+        if split[0] == "Beacon value":
             number=split[1].lower()
         elif split[0] == "Witness":
             witness=split[1].lower()
-        elif split[0] == "Timed Commitment":
+        elif split[0] == "Commitment":
             commitment=split[1].lower()
-        elif split[0] == "COMMIT_N":
+        elif split[0] == "n":
             COMMIT_N=split[1].lower()
-        elif split[0] == "COMMIT_P":
-            COMMIT_P=split[1].lower()
-        elif split[0] == "COMMIT_Q":
-            COMMIT_Q=split[1].lower()
 
 
-if( number==0 or witness==0 or commitment==0 or COMMIT_N==0 or COMMIT_P==0 or COMMIT_Q==0):
+if( number==0 or witness==0 or commitment==0 or COMMIT_N==0):
     print("Values not read correctly, check the file formating")
     exit()
 
@@ -56,64 +54,15 @@ if(commitment[-1]=='\n'):
     commitment=commitment[:-1]
 if(COMMIT_N[-1]=='\n'):
     COMMIT_N=COMMIT_N[:-1]
-if(COMMIT_P[-1]=='\n'):
-    COMMIT_P=COMMIT_P[:-1]
-if(COMMIT_Q[-1]=='\n'):
-    COMMIT_Q=COMMIT_Q[:-1]
-
-comp=int(COMMIT_P,16)
-comq=int(COMMIT_Q,16)
-comn=int(COMMIT_N,16)
-
-#check commit
-if( (comp*comq) != comn ):
-    print(TEXT_NOT_VALID)
-    exit()
-
-
-#using commit to get image not needed anymore
-
-#kcomm=pow(int(commitment,16), pow(2,TIMEDITERATIONS,(comp-1)*(comq-1)) ,comn)
-#kpass_num = kcomm % (2**KLENGTH)
-#kpass=np.base_repr(kpass_num,36).lower()
-
-#subprocess.run("openssl aes-256-cbc -d -in "+args.path_to_imagefile+" -out "+args.path_to_imagefile+".decrypt -pass pass:"+kpass,shell=True,check=True)
-
-
-
-#creation of seed which is SHA512( SHA512(tweetfile) || SHA512(imagefile) )
-
-#SHA512(tweetfile)
-with open(args.path_to_tweetfile,mode='rb') as tweetfile:
-    S0=hashlib.sha512(tweetfile.read()).hexdigest()
-
-#SHA512(imagefile)
-with open(args.path_to_imagefile,mode='rb') as imagefile:
-    S1=hashlib.sha512(imagefile.read()).hexdigest()
-
-
-S=S0+S1
-seed_string= hashlib.sha512( bytearray(S,"ascii") ).hexdigest()
-
-#check seed, commitment
-#if( hashlib.sha512( bytearray(seed_string,"ascii") ).hexdigest() != commitment):
-#    print(TEXT_NOT_VALID)
-#    exit()
-
-
-#check number, witness
-if( hashlib.sha512( bytearray(witness,"ascii") ).hexdigest() != number ):
-    print(TEXT_NOT_VALID)
-    exit()
 
 
 ################################## function def #######################
 
 # Miller-Rabin primality test copy from check for eliptic  curve
 def is_probable_prime(n, k = 25):
-
+    
     assert n >= 2, "Error in is_probable_prime: input (%d) is < 2" % n
-
+    
     # First check if n is divisible by any of the prime numbers < 1000
     low_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
                   59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
@@ -129,7 +78,7 @@ def is_probable_prime(n, k = 25):
                   797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
                   877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953,
                   967, 971, 977, 983, 991, 997]
-
+        
     for prime in low_primes:
         if (n % prime == 0):
             return False
@@ -168,18 +117,96 @@ def tho_inv(n,primemod):
         ret = pow(n,2,primemod)
     else:
         ret = primemod - pow(n,2,primemod) #((n*n) % primemod)
-
+    
     flip = (2** (primemod.bit_length()>>1) ) -1
-
+    
     ret = (ret ^ flip)
-
+    
     if( (ret >= primemod) or (ret == 0) ):
         ret = (ret ^ flip)
-
+    
     return ret
 
 
+def generate(s,i,w):
+    hexa=""
+    locali=i
+    localw=w
+    hexa=hashlib.sha512( bytearray(s+(hex(locali)[2:]),"ascii")).hexdigest()
+    while(ord(hexa[0])<ord('8')):
+        hexa=hashlib.sha512(bytearray(hexa,"ascii")).hexdigest()
+    locali+=1
+    localw-=1
+    while(localw>0):
+        hexa=hexa + hashlib.sha512( bytearray(s+(hex(locali)[2:]),"ascii")).hexdigest()
+        locali=locali+1
+        localw=localw-1
+
+    hexa=hexa[:w*128]
+    
+    intprime=int(hexa,16)
+
+    if(intprime%2 == 0):
+        intprime=intprime+1
+
+    while ( not(is_probable_prime(intprime)) ):
+        intprime=intprime+2
+
+    return intprime
+
+
+
 ######################################################################
+
+#SHA512(tweetfile)
+with open(args.path_to_tweetfile,mode='rb') as tweetfile:
+    S0=hashlib.sha512(tweetfile.read()).hexdigest()
+
+#SHA512(imagefile)
+with open(args.path_to_imagefile,mode='rb') as imagefile:
+    S1=hashlib.sha512(imagefile.read()).hexdigest()
+
+#check n
+comp=generate(S1,1,2)
+comq=generate(S1,3,2)
+comn=int(COMMIT_N,16)
+
+#check commit
+if( (comp*comq) != comn ):
+    print(TEXT_NOT_VALID)
+    exit()
+
+
+#creation of seed which is SHA512( SHA512(tweetfile) || SHA512(imagefile) ) || hex(n)
+S=hashlib.sha512( bytearray(S0+S1,"ascii") ).hexdigest()
+seed_string=S+COMMIT_N
+
+
+#check commitment
+
+compc= hashlib.sha512( bytearray(S,"ascii") ).hexdigest()
+if( compc != commitment ):
+    print(TEXT_NOT_VALID)
+    exit()
+
+
+#check encrypted image
+kcomm=pow(int(commitment,16), pow(2,TIMEDITERATIONS,(comp-1)*(comq-1)) ,comn)
+kpass_num = kcomm % (2**KLENGTH)
+kpass=np.base_repr(kpass_num,36).lower()
+
+subprocess.run("openssl aes-256-cbc -d -in "+args.path_to_encryptimagefile+" -out "+args.path_to_imagefile+".decrypt -pass pass:"+kpass,shell=True,check=True)
+if (not(filecmp.cmp(args.path_to_imagefile, args.path_to_imagefile+".decrypt", shallow=False))):
+    print(TEXT_NOT_VALID)
+    exit()
+
+
+#check number, witness
+if( hashlib.sha512( bytearray(witness,"ascii") ).hexdigest() != number ):
+    print(TEXT_NOT_VALID)
+    exit()
+
+
 
 
 
@@ -187,30 +214,17 @@ def tho_inv(n,primemod):
 nb_block=LOG_P//512
 hexa=""
 
-
-for i in range(nb_block):
-    hexa=hexa+hashlib.sha512(  bytearray(seed_string+"prime"+( chr( ord('0') +i  ) ),"ascii")  ).hexdigest()
-
-p=int(hexa,16)
-
-
-extract_msb= 2**(LOG_P-1)
-if( (p & extract_msb) == 0):
-    p=p+extract_msb
-
-if(p%2 == 0):
-    p=p+1
-else:
-    p=p+2
+p=generate(seed_string,1,nb_block)
 
 while ( (p%4 !=3) or not(is_probable_prime(p)) ):
     p=p+2
 
 
+
 #invert iteration
 hexa=""
-for i in range(nb_block):
-    hexa=hexa+hashlib.sha512(  bytearray(seed_string+"seed"+( chr( ord('0') +i  ) ),"ascii")  ).hexdigest()
+for i in range(5 ,5 + nb_block):
+    hexa=hexa + hashlib.sha512( bytearray(seed_string+(hex(i)[2:]),"ascii")).hexdigest()
 
 thoinv_seed_test = (int(hexa,16) % p)
 witness_num = int(witness,16)
